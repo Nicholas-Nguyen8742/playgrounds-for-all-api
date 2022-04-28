52f0084b591f645342960173e1e62c27718b82bb
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-04-28 19:07:33 -0400

    IndexJS structure with Port running

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+
+// Configuration 
+import 'dotenv/config'
+const PORT = process.env.PORT || 8080
+
+// Invoke cors middleware to allow requests from the browser
+app.use(cors());
+app.use(express.json());
+
+
+// Define middleware 
+app.use((req, res, next) => {
+    console.log("Incoming Request !!!");
+
+    // next() to move on to next middleware
+    next();
+})
+
+app.get("/", (req, res)=> {
+    res.send("Welcome to Nick's API XDD");
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log("App is running at port" + PORT)
+});
